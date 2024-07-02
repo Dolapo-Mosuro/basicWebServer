d1d8d95c41e378df1276b246564bf3d739a35a40
dolapo-mosuro <mosurodolapo@gmail.com>
2024-07-02 12:05:27 +0100

    test

--- app.js
+++ app.js
@@ -9,64 +9,64 @@ app.get("/", (req, res) => {
 	res.send("Welcome to the API");
 });
 
-// Handling the /api/hello Route
-app.get("/api/hello", async (req, res) => {
-	const guestName = req.query.visitor_name || "Guest";
-	const guestIp =
-		req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+// // Handling the /api/hello Route
+// app.get("/api/hello", async (req, res) => {
+// 	const guestName = req.query.visitor_name || "Guest";
+// 	const guestIp =
+// 		req.headers["x-forwarded-for"] || req.connection.remoteAddress;
 
-	try {
-		const weatherApiKey = process.env.WEATHER_API_KEY;
+// 	try {
+// 		const weatherApiKey = process.env.WEATHER_API_KEY;
 
-		console.log(
-			`Fetching weather data for IP: ${guestIp} with API key: ${weatherApiKey}`
-		);
-		const weatherResponse = await getWeatherAndLocation(guestIp, weatherApiKey);
+// 		console.log(
+// 			`Fetching weather data for IP: ${guestIp} with API key: ${weatherApiKey}`
+// 		);
+// 		const weatherResponse = await getWeatherAndLocation(guestIp, weatherApiKey);
 
-		if (!weatherResponse.location || !weatherResponse.current) {
-			throw new Error("Invalid response from WeatherAPI");
-		}
-		const location = `${weatherResponse.location.name},${weatherResponse.location.country}`;
-		const temperature = `${weatherResponse.current.temp_c.toFixed(
-			1
-		)} degrees Celsius`;
+// 		if (!weatherResponse.location || !weatherResponse.current) {
+// 			throw new Error("Invalid response from WeatherAPI");
+// 		}
+// 		const location = `${weatherResponse.location.name},${weatherResponse.location.country}`;
+// 		const temperature = `${weatherResponse.current.temp_c.toFixed(
+// 			1
+// 		)} degrees Celsius`;
 
-		const responseObj = {
-			client_ip: guestIp,
-			location: location,
-			greeting: `Hello, ${guestName}! The temperature is ${temperature} in ${location}`,
-		};
-		res.json(responseObj);
-	} catch (error) {
-		console.error(`Error fetching data: ${error.message}`);
-		res.status(500).json({ error: "Failed to fetch data" });
-	}
-});
+// 		const responseObj = {
+// 			client_ip: guestIp,
+// 			location: location,
+// 			greeting: `Hello, ${guestName}! The temperature is ${temperature} in ${location}`,
+// 		};
+// 		res.json(responseObj);
+// 	} catch (error) {
+// 		console.error(`Error fetching data: ${error.message}`);
+// 		res.status(500).json({ error: "Failed to fetch data" });
+// 	}
+// });
 
-// Fetching Weather Data using WeatherAPI
-function getWeatherAndLocation(ip, apiKey) {
-	return new Promise((resolve, reject) => {
-		const apiLocator = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${ip}`;
-		https
-			.get(apiLocator, (resp) => {
-				let data = "";
-				resp.on("data", (chunk) => {
-					data += chunk;
-				});
-				resp.on("end", () => {
-					try {
-						const weatherData = JSON.parse(data);
-						resolve(weatherData);
-					} catch (error) {
-						reject(new Error(`Failed to parse weather data: ${error.message}`));
-					}
-				});
-			})
-			.on("error", (error) => {
-				reject(new Error(`HTTP request error: ${error.message}`));
-			});
-	});
-}
+// // Fetching Weather Data using WeatherAPI
+// function getWeatherAndLocation(ip, apiKey) {
+// 	return new Promise((resolve, reject) => {
+// 		const apiLocator = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${ip}`;
+// 		https
+// 			.get(apiLocator, (resp) => {
+// 				let data = "";
+// 				resp.on("data", (chunk) => {
+// 					data += chunk;
+// 				});
+// 				resp.on("end", () => {
+// 					try {
+// 						const weatherData = JSON.parse(data);
+// 						resolve(weatherData);
+// 					} catch (error) {
+// 						reject(new Error(`Failed to parse weather data: ${error.message}`));
+// 					}
+// 				});
+// 			})
+// 			.on("error", (error) => {
+// 				reject(new Error(`HTTP request error: ${error.message}`));
+// 			});
+// 	});
+// }
 
 // Starting the Server
 app.listen(PORT, () => {
